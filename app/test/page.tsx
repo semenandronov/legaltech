dc46447c51bb5a3135ffc79cea9e016528e5ed97
Семен <semyon_andronov04@MacBook-Air-Semen.local>
2025-12-14 16:47:43 +0300

    Добавлена тестовая страница и инструкция по проверке сборки

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Test Page Works!</h1>
+      <p>If you see this, routing is working.</p>
+    </div>
+  );
+}
+
